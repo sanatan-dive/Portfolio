07dc68e1c24843ad91d275f22c2f579a465476af
sanatan-dive <sanatansharma352@gmail.com>
2024-10-21 23:59:07 +0530

    updated a project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { PROJECTS } from '../constants'
 import {animate, motion, stagger} from "framer-motion"
+import { FaGithub} from 'react-icons/fa';
 import { LuConstruction } from "react-icons/lu";
 const Projects = () => {
   return (
@@ -10,8 +11,9 @@ const Projects = () => {
         initial={{opacity:0,y:-100}}
         transition={{duration:0.5}}
         className='my-20 text-center text-4xl'>Projects</motion.h2>
-        <motion.h3 whileInView={{opacity:1,y:0}} initial={{opacity:0,y:-100}} transition={{duration:0.5}} className='text-center flex items-center justify-center flex-col gap-4 text-3xl'><LuConstruction className='text-5xl' />will update them soon</motion.h3>
-        {/* <div>
+        {/* <motion.h3 whileInView={{opacity:1,y:0}} initial={{opacity:0,y:-100}} transition={{duration:0.5}} className='text-center flex items-center justify-center flex-col gap-4 text-3xl'><LuConstruction className='text-5xl' />will update them soon</motion.h3> */}
+        <div>
+            
             {PROJECTS.map((project,index) => (
                 <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
                     <motion.div 
@@ -19,22 +21,25 @@ const Projects = () => {
                     initial={{opacity:0,x:-100}}
                     transition={{duration:1}}
                     className='w-full lg:w-1/4'>
-                        <img src={project.image} width={250} height={250} alt={project.title} className='mb-6 rounded-xl'/>
+                        <img src={project.image} width={500} height={500} alt={project.title} className='mb-6  lg:mx-[-100px] rounded-xl'/>
                     </motion.div> 
                     <motion.div
                     whileInView={{opacity:1,x:0}}
                     initial={{opacity:0,x:100}}
                     transition={{duration:1}}
                     className='w-full max-w-xl lg:w-3/4'>
-                        <h3 className='mb-2 font-semibold text-2xl'>{project.title}</h3>
+                       <h3 className='mb-2 font-semibold text-2xl'>{project.title}</h3>
                         <p className='mb-4 text-stone-400'>{project.description}</p>
+                        <a href="https://9otes-vqml.vercel.app/">
+                        <FaGithub className=' mb-5 mr-3 text-3xl text-stone-300'/>
+                        </a>
                         {project.technologies.map((technology,index) => (
                             <span className='mr-3 rounded bg-stone-900 p-2 text-sm font-medium text-stone-300'> {technology}</span>
                         ))}
                     </motion.div>
                 </div>
             ))}
-        </div> */}
+        </div>
     </div>
   )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 import project1 from "../assets/projects/project-1.webp";
-import project2 from "../assets/projects/project-2.webp";
-import project3 from "../assets/projects/project-3.webp";
-import project4 from "../assets/projects/project-4.webp";
+// import project2 from "../assets/projects/project-2.webp";
+// import project3 from "../assets/projects/project-3.webp";
+// import project4 from "../assets/projects/project-4.webp";
 
 export const HERO_CONTENT = `I am an aspiring web developer with a passion for creating robust and scalable web applications. I am building my expertise in front-end technologies like React and Next.js, along with back-end technologies such as Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to apply my growing skills to develop innovative solutions that contribute to business growth and deliver exceptional user experiences.`;
 
@@ -40,33 +40,13 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "9otes",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "an advanced todo list application that goes beyond basic functionality. It incorporates features like categories, markdown support, and filtering options, making it suitable for intermediate to advanced developers. This project is aimed at enhancing note-taking capabilities, allowing users to organize and manage their tasks more effective",
+    technologies: ["HTML", "BootStrap","TypeScript" ,"React", ],
   },
-  {
-    title: "Task Management App",
-    image: project2,
-    description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
-  },
-  {
-    title: "Portfolio Website",
-    image: project3,
-    description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-  },
+  
 ];
 
 export const CONTACT = {
