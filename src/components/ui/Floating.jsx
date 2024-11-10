61b0a8b2db28d2fd354cd8bfa5d6bb968f92b2b8
sanatan-dive <sanatansharma352@gmail.com>
2024-11-10 21:06:51 +0530

    changed nav

--- src/components/ui/Floating.jsx
+++ src/components/ui/Floating.jsx
@@ -12,7 +12,7 @@ const FloatingNav = ({ navItems = [], className }) => {
   return (
     <div
       className={cn(
-        "flex sticky justify-center items-center max-w-fit mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-stone-950 bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-5 pl-5 py-2 mt-4   space-x-4 bg-opacity-90 backdrop-blur-md",
+        "flex sticky justify-center items-center max-w-fit mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-stone-950 bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-6 pl-6 py-4  mt-4   space-x-4 bg-opacity-90 backdrop-blur-md",
         className
       )}
     >
