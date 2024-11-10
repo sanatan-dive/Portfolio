50661977ea63dd27586be93f1db83173f9438f1d
sanatan-dive <sanatansharma352@gmail.com>
2024-11-10 12:40:33 +0530

    added more projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,47 +1,92 @@
-import React from 'react'
-import { PROJECTS } from '../constants'
-import {animate, motion, stagger} from "framer-motion"
-import { FaGithub} from 'react-icons/fa';
+import React from 'react';
+import { animate, motion } from "framer-motion";
+import { FaGithub } from 'react-icons/fa';
 import { LuConstruction } from "react-icons/lu";
+import project1 from "../assets/projects/project-1.webp";
+import project2 from "../assets/projects/project-2.webp";
+
 const Projects = () => {
   return (
     <div className='pb-4'>
-        <motion.h2 
+      <motion.h2 
         whileInView={{opacity:1,y:0}}
         initial={{opacity:0,y:-100}}
         transition={{duration:0.5}}
-        className='my-20 text-center text-4xl'>Projects</motion.h2>
-        {/* <motion.h3 whileInView={{opacity:1,y:0}} initial={{opacity:0,y:-100}} transition={{duration:0.5}} className='text-center flex items-center justify-center flex-col gap-4 text-3xl'><LuConstruction className='text-5xl' />will update them soon</motion.h3> */}
-        <div>
-            
-            {PROJECTS.map((project,index) => (
-                <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
-                    <motion.div 
-                    whileInView={{opacity:1,x:0}}
-                    initial={{opacity:0,x:-100}}
-                    transition={{duration:1}}
-                    className='w-full lg:w-1/4'>
-                        <img src={project.image} width={500} height={500} alt={project.title} className='mb-6  lg:mx-[-100px] rounded-xl'/>
-                    </motion.div> 
-                    <motion.div
-                    whileInView={{opacity:1,x:0}}
-                    initial={{opacity:0,x:100}}
-                    transition={{duration:1}}
-                    className='w-full max-w-xl lg:w-3/4'>
-                       <h3 className='mb-2 font-semibold text-2xl'>{project.title}</h3>
-                        <p className='mb-4 text-stone-400'>{project.description}</p>
-                        <a target='_blank' href="https://github.com/sanatan-dive/9otes">
-                        <FaGithub className=' mb-5 mr-3 text-3xl text-stone-300'/>
-                        </a>
-                        {project.technologies.map((technology,index) => (
-                            <span className='mr-3 rounded bg-stone-900 p-2 text-sm font-medium text-stone-300'> {technology}</span>
-                        ))}
-                    </motion.div>
-                </div>
-            ))}
-        </div>
+        className='my-20 text-center text-4xl'>
+        Projects
+      </motion.h2>
+
+      {/* Project 1 */}
+      <div className='mb-8 flex flex-wrap lg:justify-center'>
+        <motion.div 
+          whileInView={{opacity:1,x:0}}
+          initial={{opacity:0,x:-100}}
+          transition={{duration:1}}
+          className='w-full lg:w-1/4'>
+            <a href='https://9otes.vercel.app/'>
+          <img src={project1} width={500} height={500} alt="9otes" className='mb-6 lg:mx-[-100px] rounded-xl'/>
+          </a>
+        </motion.div>
+        <motion.div
+          whileInView={{opacity:1,x:0}}
+          initial={{opacity:0,x:100}}
+          transition={{duration:1}}
+          className='w-full max-w-xl lg:w-3/4'>
+          <a href='https://9otes.vercel.app/'>
+          <h3 className='mb-2 font-semibold text-2xl'>9otes</h3>
+          </a>
+          <p className='mb-4 text-stone-400'>
+            An advanced note take application that goes beyond basic functionality. It incorporates features like categories, markdown support, and filtering options, making it suitable for intermediate to advanced developers. This project is aimed at enhancing note-taking capabilities, allowing users to organize and manage their tasks more effectively.
+          </p>
+          <a className='text-2xl  '  href='https://github.com/sanatan-dive/9otes' target='_blank'>
+            <FaGithub />
+          </a>
+
+          <div className='mt-4 flex flex-wrap'>
+          <span className='mr-3 rounded bg-stone-900 p-2 text-sm font-medium text-stone-300'>HTML</span>
+          <span className='mr-3 rounded bg-stone-900 p-2 text-sm font-medium text-stone-300'>BootStrap</span>
+          <span className='mr-3 rounded bg-stone-900 p-2 text-sm font-medium text-stone-300'>TypeScript</span>
+          <span className='mr-3 rounded bg-stone-900 p-2 text-sm font-medium text-stone-300'>React</span>
+          </div>
+        </motion.div>
+      </div>
+
+      {/* Project 2 */}
+      <div className='mb-8 flex flex-wrap lg:justify-center'>
+        <motion.div 
+          whileInView={{opacity:1,x:0}}
+          initial={{opacity:0,x:-100}}
+          transition={{duration:1}}
+          className='w-full lg:w-1/4'>
+            <a href='https://live-dox.vercel.app/'>
+            <img src={project2} width={500} height={500} alt="LiveDox" className='mb-6 lg:mx-[-100px] rounded-xl'/>
+            </a>
+         
+        </motion.div>
+        <motion.div
+          whileInView={{opacity:1,x:0}}
+          initial={{opacity:0,x:100}}
+          transition={{duration:1}}
+          className='w-full max-w-xl lg:w-3/4'>
+          <a href='https://live-dox.vercel.app/'>
+          <h3 className='mb-2 font-semibold text-2xl'>LiveDox</h3>
+          </a>
+          <p className='mb-4 text-stone-400'>
+            A collaborative docs editor which lets you edit your docs and invite other people to edit and maintain documents together.
+          </p>
+          <a className='text-2xl  '  href='https://github.com/sanatan-dive/LiveDox' target='_blank'>
+            <FaGithub />
+          </a>
+          <div className='mt-4 flex flex-wrap'>
+          <span className='mr-3 rounded bg-stone-900 p-2 text-sm font-medium text-stone-300'>LiveBlocks</span>
+          <span className='mr-3 rounded bg-stone-900 p-2 text-sm font-medium text-stone-300'>Tailwind</span>
+          <span className='mr-3 rounded bg-stone-900 p-2 text-sm font-medium text-stone-300'>TypeScript</span>
+          <span className='mr-3 rounded bg-stone-900 p-2 text-sm font-medium text-stone-300'>NextJS</span>
+          </div>
+        </motion.div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
