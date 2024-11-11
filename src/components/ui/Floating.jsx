09a8b0f04941047a047c4f1c30169005c221dde2
sanatan-dive <sanatansharma352@gmail.com>
2024-11-11 15:57:27 +0530

    navbar is now floating dock

--- src/components/ui/Floating.jsx
+++ src/components/ui/Floating.jsx
@@ -23,14 +23,19 @@ const FloatingNav = ({ navItems = [], className }) => {
           className={cn(
             "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 justify-center dark:hover:text-neutral-300 hover:text-neutral-500 transition-all duration-300",
             "group", // Group for hover effect
-            idx !== navItems.length - 1 ? "border-r border-neutral-300 dark:border-neutral-700 pr-4" : "pr-0"
+            idx !== navItems.length - 1 ? "border-r border-neutral-300 dark:border-neutral-700 pr-3" : "pr-0"
           )}
         >
           <span className="block sm:hidden">{navItem.icon}</span>
           <span className="flex flex-wrap sm:block text-sm">
             {navItem.name}
           </span>
-          <div className="absolute bottom-0 left-0 w-full h-[2px] bg-gradient-to-r from-transparent via-blue-500 to-transparent scale-x-0 group-hover:scale-x-100 transition-all duration-300"></div>
+          <div
+  className="absolute bottom-0 left-0 right-0 w-full h-[2px] bg-gradient-to-r from-transparent via-blue-500 to-transparent scale-x-0 group-hover:scale-x-100 transition-transform duration-300"
+></div>
+
+
+
         </a>
       ))}
     </div>
